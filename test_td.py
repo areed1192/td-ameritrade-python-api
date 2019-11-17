aacd09328b40cdd357d45f1de8a961077ed60f19
Alex Reed <alexreed1192@gmail.com>
2019-11-17 14:39:08 -0800

    Add Test Td File

---
+++ test_td.py
@@ -0,0 +1,11 @@
+from td.client import TDClient
+from td.saved_order import OrderLeg, SavedOrder
+from td.enums import ORDER_SESSION, ORDER_TYPE
+
+# from config import ACCOUNT_NUMBER, ACCOUNT_PASSWORD, CONSUMER_ID, REDIRECT_URI, TD_ACCOUNT, TRANSACTION_ID
+
+# # create a new session
+# TDSession = TDClient(account_number = ACCOUNT_NUMBER,
+#                      account_password = ACCOUNT_PASSWORD,
+#                      consumer_id = CONSUMER_ID,
+#                      redirect_uri = REDIRECT_URI)
