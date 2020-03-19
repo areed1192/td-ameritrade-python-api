bb802c1f2bb106d9c45505b9328a04ae86d1c1ff
Alex Reed <alexreed1192@gmail.com>
2020-03-19 13:10:08 -0700

    Add Alex Testing files mods

--- tests/test_streaming_client.py
+++ tests/test_streaming_client.py
@@ -60,13 +60,13 @@ from td.client import TDClient
 
 TESTING_FLAG = True
 
-# if TESTING_FLAG:
-#     from config import ACCOUNT_NUMBER, ACCOUNT_PASSWORD, CONSUMER_ID, REDIRECT_URI, TD_ACCOUNT
-# else:
-ACCOUNT_NUMBER = '<YOUR TD ACCOUNT USERNAME>'
-ACCOUNT_PASSWORD = '<YOUR TD ACCOUNT PASSWORD>'
-CONSUMER_ID = '<YOUR TD DEVELOPER ACCOUNT CONSUMER ID>'
-REDIRECT_URI = '<YOUR TD DEVELOPER ACCOUNT REDIRECT URI>'
+if TESTING_FLAG:
+    from td.config import ACCOUNT_NUMBER, ACCOUNT_PASSWORD, CONSUMER_ID, REDIRECT_URI, TD_ACCOUNT
+else:
+    ACCOUNT_NUMBER = '<YOUR TD ACCOUNT USERNAME>'
+    ACCOUNT_PASSWORD = '<YOUR TD ACCOUNT PASSWORD>'
+    CONSUMER_ID = '<YOUR TD DEVELOPER ACCOUNT CONSUMER ID>'
+    REDIRECT_URI = '<YOUR TD DEVELOPER ACCOUNT REDIRECT URI>'
 
 # Create a new session
 TDSession = TDClient(account_number=ACCOUNT_NUMBER,
@@ -87,11 +87,11 @@ TDStreamingClient.CSV_APPEND_MODE = True
     REGULAR - WORKING
 '''
 
-# Actives
-TDStreamingClient.actives(service='ACTIVES_NASDAQ', venue='NASDAQ', duration='ALL')
+# # Actives
+# TDStreamingClient.actives(service='ACTIVES_NASDAQ', venue='NASDAQ', duration='ALL')
 
-# Quality of Service
-TDStreamingClient.quality_of_service(qos_level='express')
+# # Quality of Service
+# TDStreamingClient.quality_of_service(qos_level='express')
 
 '''
     LEVEL ONE DATA
@@ -100,79 +100,79 @@ TDStreamingClient.quality_of_service(qos_level='express')
 # Level One Quote
 TDStreamingClient.level_one_quotes(symbols=["SPY", "IVV", "SDS", "SH", "SPXL", "SPXS", "SPXU", "SSO", "UPRO", "VOO"],  fields=list(range(0,8)))
 
-# Level One Option
-TDStreamingClient.level_one_options(symbols=['MSFT_030620P140'], fields=list(range(0,42)))
+# # Level One Option
+# TDStreamingClient.level_one_options(symbols=['MSFT_030620P140'], fields=list(range(0,42)))
 
-# Level One Futures
-TDStreamingClient.level_one_futures(symbols=['/CL'], fields=["0", "1", "2", "3", "4"])
+# # Level One Futures
+# TDStreamingClient.level_one_futures(symbols=['/CL'], fields=["0", "1", "2", "3", "4"])
 
-# Level One Forex - VALIDATE JSON RESPONSE
-TDStreamingClient.level_one_forex(symbols=['EUR/USD'], fields=list(range(0,26)))
+# # Level One Forex - VALIDATE JSON RESPONSE
+# TDStreamingClient.level_one_forex(symbols=['EUR/USD'], fields=list(range(0,26)))
 
-# Level One Futures Options - VALIDATE JSON RESPONSE
-TDStreamingClient.level_one_futures_options(symbols=['./E1AG20C3220'], fields=list(range(0,36)))
+# # Level One Futures Options - VALIDATE JSON RESPONSE
+# TDStreamingClient.level_one_futures_options(symbols=['./E1AG20C3220'], fields=list(range(0,36)))
 
-# Charts, this looks like it only streams every one minute. Hence if you want the last bar you should use this.
-TDStreamingClient.chart(service='CHART_FUTURES', symbols=['/CL'], fields=[0,1,2,3,4,5,6,7])
+# # Charts, this looks like it only streams every one minute. Hence if you want the last bar you should use this.
+# TDStreamingClient.chart(service='CHART_FUTURES', symbols=['/CL'], fields=[0,1,2,3,4,5,6,7])
 
-# Charts, this looks like it only streams every one minute. Hence if you want the last bar you should use this.
-TDStreamingClient.chart(service='CHART_OPTIONS', symbols=['MSFT_030620P140'], fields=[0,1,2,3,4,5,6])
+# # Charts, this looks like it only streams every one minute. Hence if you want the last bar you should use this.
+# TDStreamingClient.chart(service='CHART_OPTIONS', symbols=['MSFT_030620P140'], fields=[0,1,2,3,4,5,6])
 
-# Chart History Futures
-TDStreamingClient.chart_history_futures(symbol = ['/ES'], frequency='m5', period='d1')
+# # Chart History Futures
+# TDStreamingClient.chart_history_futures(symbol = ['/ES'], frequency='m5', period='d1')
 
-# Timesale
-TDStreamingClient.timesale(service='TIMESALE_FUTURES', symbols=['/ES'], fields=[0, 1, 2, 3, 4])
+# # Timesale
+# TDStreamingClient.timesale(service='TIMESALE_FUTURES', symbols=['/ES'], fields=[0, 1, 2, 3, 4])
 
-'''
-    Hard to Identify what fixed the inital error.
-    ---------------------------------------------
-    1st. I set Streaming News to on for both of my accounts and the inital result was nothing.
-    2nd. I opened ToS and then made a request to this endpoint and go success. However, even after closing it I still go a request.
+# '''
+#     Hard to Identify what fixed the inital error.
+#     ---------------------------------------------
+#     1st. I set Streaming News to on for both of my accounts and the inital result was nothing.
+#     2nd. I opened ToS and then made a request to this endpoint and go success. However, even after closing it I still go a request.
 
-    It's possible that step one fixed the issue, but there is a delay before you start seeing anything? Maybe a 15 minute delay? Additionally,
-    I only had it on for one of my accounts and not the other, so you may need to turn it on for the account that is the main one you use.
-'''
+#     It's possible that step one fixed the issue, but there is a delay before you start seeing anything? Maybe a 15 minute delay? Additionally,
+#     I only had it on for one of my accounts and not the other, so you may need to turn it on for the account that is the main one you use.
+# '''
 
-# News Headline
-TDStreamingClient.news_headline(symbols=['AAPL'], fields=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+# # News Headline
+# TDStreamingClient.news_headline(symbols=['AAPL'], fields=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-'''
-    The Documentation makes this one confusing.
-    ---------------------------------------------
-    The documentation kept mentioning something related to a MessageKey API. The problem was there was no reference to it anywhere
-    on the TD Ameritrade website. It appears it was an old endpoint in the old API. However, the documentation seems to not reflect
-    the new protocol for subscribing to this stream.
+# '''
+#     The Documentation makes this one confusing.
+#     ---------------------------------------------
+#     The documentation kept mentioning something related to a MessageKey API. The problem was there was no reference to it anywhere
+#     on the TD Ameritrade website. It appears it was an old endpoint in the old API. However, the documentation seems to not reflect
+#     the new protocol for subscribing to this stream.
 
-    When you get the streaming key, this appears to also be the MessageKey needed for this request. In other words, to use this endpoint
+#     When you get the streaming key, this appears to also be the MessageKey needed for this request. In other words, to use this endpoint
 
-    1.Make a request to either the "Get User Principals" endpoint or the "Get Streamer Subscription Keys" endpoint and grab the subscription
-      key from that one.
-    2.Use the Subscription Key from that request as the "Keys" argument for the request.
-'''
+#     1.Make a request to either the "Get User Principals" endpoint or the "Get Streamer Subscription Keys" endpoint and grab the subscription
+#       key from that one.
+#     2.Use the Subscription Key from that request as the "Keys" argument for the request.
+# '''
 
-# Account Activity
-TDStreamingClient.account_activity()
+# # Account Activity
+# TDStreamingClient.account_activity()
 
 
-'''
-    EXPERIMENTAL SECTION
-'''
+# '''
+#     EXPERIMENTAL SECTION
+# '''
 
-# Level Two Options
-TDStreamingClient.level_two_options(symbols=['ESH20_022120C20'], fields = [0,1,2])
+# # Level Two Options
+# TDStreamingClient.level_two_options(symbols=['ESH20_022120C20'], fields = [0,1,2])
 
-# Level Two Quotes
-TDStreamingClient.level_two_quotes(symbols = ['IBM'], fields = [0,1,2])
+# # Level Two Quotes
+# TDStreamingClient.level_two_quotes(symbols = ['IBM'], fields = [0,1,2])
 
-# Level Two NASQDAQ
-TDStreamingClient.level_two_nasdaq(symbols = ['MSFT'], fields = [0,1,2])
+# # Level Two NASQDAQ
+# TDStreamingClient.level_two_nasdaq(symbols = ['MSFT'], fields = [0,1,2])
 
-# Level Two NYSE
-TDStreamingClient.level_two_nyse(symbols = ['AA'], fields = [0,1,2])
+# # Level Two NYSE
+# TDStreamingClient.level_two_nyse(symbols = ['AA'], fields = [0,1,2])
 
-# Level Two Total View 
-TDStreamingClient.level_two_total_view(symbols = ['AAPL'], fields = [0,1,2])
+# # Level Two Total View 
+# TDStreamingClient.level_two_total_view(symbols = ['AAPL'], fields = [0,1,2])
 
 
 # # Level Two Futures - NOT WORKING - MAY WORK IF YOU HAVE FUTURES TRADING ENABLED ON YOUR ACCOUNT.
