b990c061b72ddfb73f8167dd15ac769852de1495
Alex Reed <alexreed1192@gmail.com>
2020-04-15 15:08:20 -0700

    Add new doc strings and type hinting.
    You wouldn't have known this, but I'm in the process of changing all the docstrings so they'll display in visual studio code with formatting. Additionally, I'm adding type hinting to all the functions.

--- td/utils.py
+++ td/utils.py
@@ -1,24 +1,33 @@
 import datetime
 
+def milliseconds_since_epoch(dt_object: datetime.datetime) -> int:
+    """converts a datetime object to milliseconds since 1970, as an integer
+    
+    Arguments:
+    ----------
+        dt_object {datetime.datetime} -- Python datetime object.
+    
+    Returns:
+    --------
+        [int] -- The timestamp in milliseconds since epoch.
+    """
 
-def milliseconds_since_epoch(dt_object):
-    '''
-        converts a datetime object to milliseconds since 1970, as an integer
+    return int(dt_object.timestamp() * 1000)
 
-        NAME: dt_object
-        DESC: python datetime object
-        TYPE: datetime.datetime
-    '''
-    return int( dt_object.timestamp() * 1e3 )
+def datetime_from_milliseconds_since_epoch(ms_since_epoch: int, timezone: datetime.timezone = None) -> datetime.datetime:
+    """Converts milliseconds since epoch to a datetime object.
+    
+    Arguments:
+    ----------
+        ms_since_epoch {int} -- Number of milliseconds since epoch.
+    
+    Keyword Arguments:
+    --------
+        timezone {datetime.timezone} -- The timezone of the new datetime object. (default: {None})
+    
+    Returns:
+    --------
+        datetime.datetime -- A python datetime object.
+    """
 
-
-def datetime_from_milliseconds_since_epoch(ms_since_epoch,timezone=None):
-    '''
-        converts milliseconds since epoch to a datetime object
-
-        NAME: ms_since_epoch
-        DESC: milliseconds since epoch
-        TYPE: Numbers.number
-    '''
-
-    return datetime.datetime.fromtimestamp(ms_since_epoch/1e3, tz=timezone)
+    return datetime.datetime.fromtimestamp((ms_since_epoch / 1000), tz=timezone)
